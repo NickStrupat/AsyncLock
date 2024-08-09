bce77bf7a2764a58188b5eeeb652d351a7058724
NickStrupat <nick.strupat@gmail.com>
2024-08-09 02:00:31 -0400

    abstract the accessing of the shared Task and the shard TaskCompletionSource to more clearly show when potentially stale data is being accessed, versus when atomic exchanges happen.

--- src/AsyncLock/AsyncLock.cs
+++ src/AsyncLock/AsyncLock.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace NickStrupat;
@@ -9,13 +8,13 @@ public sealed class AsyncLock
 	#if DEBUG
 	public UInt64 TcsCtorCount;
 	#endif
-	private Task task = Task.CompletedTask;
+	private Interlocked<Task> task = new(Task.CompletedTask);
 
 	public async ValueTask LockAsync(Func<Task> whenLocked)
 	{
 		ArgumentNullException.ThrowIfNull(whenLocked);
 		var next = Get();
-		var prev = Interlocked.Exchange(ref task, next.Task);
+		var prev = task.Exchange(next.Task);
 		await prev.ConfigureAwait(false);
 		try
 		{
@@ -23,28 +22,29 @@ public sealed class AsyncLock
 		}
 		finally
 		{
-			if (Interlocked.CompareExchange(ref task, prev, next.Task) == next.Task)
+			if (task.CompareExchange(prev, next.Task) == next.Task)
 				Return(next);
 			else
 				next.SetResult();
 		}
 	}
 
-	private TaskCompletionSource? fastTcs;
+	private Interlocked<TaskCompletionSource?> cachedTcs;
+	
 	private TaskCompletionSource Get()
 	{
-		var item = fastTcs;
-		if (item != null && Interlocked.CompareExchange(ref fastTcs, null, item) == item)
+		var item = cachedTcs.CoreCachedValue;
+		if (item != null && cachedTcs.CompareExchange(null, item) == item)
 			return item;
 		#if DEBUG
 		Interlocked.Increment(ref TcsCtorCount);
 		#endif
-		return new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+		return new(TaskCreationOptions.RunContinuationsAsynchronously);
 	}
 
 	private void Return(TaskCompletionSource tcs)
 	{
-		if (fastTcs == null)
-			Interlocked.CompareExchange(ref fastTcs, tcs, null);
+		if (cachedTcs.CoreCachedValue == null)
+			cachedTcs.CompareExchange(tcs, null);
 	}
 }
